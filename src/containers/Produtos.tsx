d3d2d997da8f602ad1bb82f998b72c5705def619
Erick Staviasz <erickesc21@hotmail.com>
2023-05-16 11:46:26 -0300

    converssão de useState() para o redux

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,19 +1,18 @@
-import { Game } from '../App'
 import Produto from '../components/Produto'
 
+import { useGetJogosQuery } from '../services/apis'
 import * as S from './styles'
 
-type Props = {
-  jogos: Game[]
-  adicionarAoCarrinho: (jogo: Game) => void
-}
+const Produtos = () => {
+  const { data: jogos, isLoading } = useGetJogosQuery()
 
-const Produtos = ({ jogos, adicionarAoCarrinho }: Props) => {
+  if (isLoading) return <h2>Carregango...</h2>
+
   return (
     <>
       <S.Produtos>
-        {jogos.map((game) => (
-          <Produto key={game.id} game={game} aoComprar={adicionarAoCarrinho} />
+        {jogos?.map((game) => (
+          <Produto key={game.id} game={game} />
         ))}
       </S.Produtos>
     </>
---
+++ src/store/reducers/carrinho.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+import { Game } from '../../App'
+
+type CarrinhoState = {
+  itens: Game[]
+}
+
+const initialState: CarrinhoState = {
+  itens: []
+}
+
+const carrinhoSlice = createSlice({
+  name: 'carrinho',
+  initialState,
+  reducers: {
+    adicionarAoCarrinho: (state, action: PayloadAction<Game>) => {
+      const jogo = action.payload
+
+      if (state.itens.find((game) => game.id === jogo.id)) {
+        alert('Item já dicionado')
+      } else {
+        state.itens.push(jogo)
+      }
+    }
+  }
+})
+
+export const { adicionarAoCarrinho } = carrinhoSlice.actions
+export default carrinhoSlice.reducer
